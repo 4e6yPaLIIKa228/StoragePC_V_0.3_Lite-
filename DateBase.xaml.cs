8a932f1d509417992517eb01990750bb0aeeb14c
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2021-10-07 14:46:01 +0300

    Buba
    Boba

---
+++ DateBase.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoragePC
+{
+    /// <summary>
+    /// Логика взаимодействия для DateBase.xaml
+    /// </summary>
+    public partial class DateBase : Window
+    {
+            public Device Device { get; private set; }
+
+            public DateBase(Device p)
+            {
+                InitializeComponent();
+                Device = p;
+                this.DataContext = Device;
+            }
+
+            private void Accept_Click(object sender, RoutedEventArgs e)
+            {
+                this.DialogResult = true;
+            }
+    }
+
+}
